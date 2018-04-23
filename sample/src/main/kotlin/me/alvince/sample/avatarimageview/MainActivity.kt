3bf3656b1e28438017f8bb4ae23cd57e043ad2a6
alvince <alvince.zy@gmail.com>
2018-04-23 16:47:52 +0800

    Add images display to sample

--- sample/src/main/kotlin/me/alvince/sample/avatarimageview/MainActivity.kt
+++ sample/src/main/kotlin/me/alvince/sample/avatarimageview/MainActivity.kt
@@ -2,11 +2,25 @@ package me.alvince.sample.avatarimageview
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
+import me.alvince.sample.avatarimageview.adapter.SampleGalleryAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = SampleGalleryAdapter(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Glide.with(this)
+                .load(getString(R.string.steins_gate_poster))
+                .into(avatarImageView)
     }
 }
--- sample/src/main/kotlin/me/alvince/sample/avatarimageview/adapter/SampleGalleryAdapter.kt
+++ sample/src/main/kotlin/me/alvince/sample/avatarimageview/adapter/SampleGalleryAdapter.kt
@@ -1,30 +1,73 @@
 package me.alvince.sample.avatarimageview.adapter
 
+import android.content.Context
+import android.graphics.Rect
 import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.util.TypedValue
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import me.alvince.android.avatarimageview.AvatarImageView
+import me.alvince.sample.avatarimageview.R
 
 /**
  * Created by alvince on 2018/4/18
  *
  * @author 杨小咩 alvince.zy@gmail.com
  */
-class SampleGalleryAdapter : RecyclerView.Adapter<ViewHolder>() {
+class SampleGalleryAdapter(context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        const val TAG = "SampleGalleryAdapter"
+    }
 
     private val dataSource: MutableList<String> = ArrayList()
+    private val imageSize: Float
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    init {
+        dataSource.addAll(context.resources.getStringArray(R.array.steins_gate))
+        imageSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 128F, context.resources.displayMetrics)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val image = AvatarImageView(parent.context)
-        return ViewHolder(image)
+        image.layoutParams = ViewGroup.LayoutParams(imageSize.toInt(), imageSize.toInt())
+        image.scaleType = ImageView.ScaleType.CENTER
+        return SampleViewHolder(image)
     }
 
     override fun getItemCount(): Int {
         return dataSource.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder.itemView) {
+            is AvatarImageView -> {
+                Glide.with(holder.itemView.context)
+                        .load(dataSource[position])
+                        .into(holder.itemView as ImageView)
+            }
+            else -> Log.d(TAG, dataSource[position])
+        }
+    }
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        recyclerView.addItemDecoration(ItemDecoration(recyclerView.context))
+    }
+
+
+    internal class SampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    internal class ItemDecoration(context: Context): RecyclerView.ItemDecoration() {
+
+        val offset = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8F, context.resources.displayMetrics).toInt()
+
+        override fun getItemOffsets(outRect: Rect?, view: View?, parent: RecyclerView?, state: RecyclerView.State?) {
+            outRect?.left = offset
+            outRect?.right = offset
+        }
     }
 }
-
-class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
