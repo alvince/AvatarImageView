e8fa6dc26c4beb02d6d5b37b9983dd84fbf9531d
alvince <alvince.zy@gmail.com>
2018-08-01 12:27:53 +0800

    Optimize params appearance

--- sample/src/main/kotlin/me/alvince/sample/avatarimageview/dialog/ParamsDialog.kt
+++ sample/src/main/kotlin/me/alvince/sample/avatarimageview/dialog/ParamsDialog.kt
@@ -2,11 +2,17 @@ package me.alvince.sample.avatarimageview.dialog
 
 import android.content.Context
 import android.content.res.Resources
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.support.design.widget.BottomSheetDialog
 import android.text.Editable
+import android.text.Spannable
+import android.text.SpannableString
 import android.text.TextWatcher
+import android.text.style.StyleSpan
 import android.util.TypedValue
+import android.view.WindowManager
 import com.dingmouren.colorpicker.ColorPickerDialog
 import com.dingmouren.colorpicker.OnColorPickerListener
 import kotlinx.android.synthetic.main.dialog_view_params.*
@@ -23,20 +29,30 @@ import me.alvince.sample.avatarimageview.convert2DpInt
 class ParamsDialog(context: Context, val data: Params, private val listener: (Params) -> Unit)
     : BottomSheetDialog(context) {
 
+    private var circular = false
+    private var cornerRadius = 0F
+    private var strokeWidth = 0F
+    private var strokeColor = Color.TRANSPARENT
+
+    init {
+        circular = data.roundAsCircle
+        cornerRadius = data.roundedCorner.toFloat()
+        strokeWidth = data.strokeWidth.toFloat()
+        strokeColor = data.strokeColor
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_view_params)
-
-        block_color.setOnClickListener { showColorPicker() }
-        text_label_color.setOnClickListener { showColorPicker() }
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT)
 
         edit_corner_radius.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
                 if (s == null || s.isEmpty()) {
                     return
                 }
-                data.roundedCorner = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
-                        s.toString().toFloat(), Resources.getSystem().displayMetrics).toInt()
+                cornerRadius = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                        s.toString().toFloat(), Resources.getSystem().displayMetrics)
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -50,8 +66,8 @@ class ParamsDialog(context: Context, val data: Params, private val listener: (Pa
                 if (s == null || s.isEmpty()) {
                     return
                 }
-                data.strokeWidth = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
-                        s.toString().toFloat(), Resources.getSystem().displayMetrics).toInt()
+                strokeWidth = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                        s.toString().toFloat(), Resources.getSystem().displayMetrics)
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -60,24 +76,36 @@ class ParamsDialog(context: Context, val data: Params, private val listener: (Pa
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
             }
         })
-
-        setOnDismissListener { listener.invoke(data) }
+        block_color.setOnClickListener { showColorPicker() }
+        text_label_color.setOnClickListener { showColorPicker() }
+        btn_negative.setOnClickListener { dismiss() }
+        btn_positive.setOnClickListener {
+            if (validate()) {
+                listener.invoke(data)
+            }
+            dismiss()
+        }
     }
 
     override fun onStart() {
         super.onStart()
-        text_url.text = "URL: ${data.url}"
-        check_circular.isChecked = data.roundAsCircle
+        val url = SpannableString("URL [ ${data.url} ]")
+        val urlStart = url.indexOf('[').inc()
+        val urlEnd = url.lastIndexOf(']')
+        url.setSpan(StyleSpan(Typeface.BOLD), 0, 3, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+        url.setSpan(StyleSpan(Typeface.ITALIC), urlStart, urlEnd, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+        text_url.text = url
+        check_circular.isChecked = circular
         check_circular.isEnabled = false
-        block_color.setBackgroundColor(data.strokeColor)
-        edit_corner_radius.setText("${convert2DpInt(data.roundedCorner.toFloat())}")
-        edit_stroke_width.setText("${convert2DpInt(data.strokeWidth.toFloat())}")
+        block_color.setBackgroundColor(strokeColor)
+        edit_corner_radius.setText("${convert2DpInt(cornerRadius)}")
+        edit_stroke_width.setText("${convert2DpInt(strokeWidth)}")
     }
 
     private fun showColorPicker() {
-        val picker = ColorPickerDialog(context, data.strokeColor, object : OnColorPickerListener {
+        val picker = ColorPickerDialog(context, strokeColor, object : OnColorPickerListener {
             override fun onColorConfirm(dialog: ColorPickerDialog?, color: Int) {
-                data.strokeColor = color
+                strokeColor = color
                 block_color.setBackgroundColor(color)
             }
 
@@ -85,9 +113,27 @@ class ParamsDialog(context: Context, val data: Params, private val listener: (Pa
             }
 
             override fun onColorChange(dialog: ColorPickerDialog?, color: Int) {
-                data.strokeColor = color
             }
         })
         picker.show()
+    }
+
+    private fun validate(): Boolean {
+        var changed = false
+        if (data.strokeColor != strokeColor) {
+            data.strokeColor = strokeColor
+            changed = true
+        }
+        val strokeWidthPix = strokeWidth.toInt()
+        if (data.strokeWidth != strokeWidthPix) {
+            data.strokeWidth = strokeWidthPix
+            changed = true
+        }
+        val radiusPix = cornerRadius.toInt()
+        if (data.roundedCorner != radiusPix) {
+            data.roundedCorner = radiusPix
+            changed = true
+        }
+        return changed
     }
 }
